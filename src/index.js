c71b2aaa119f03963b7143a8bddf0a61697a0cc4
Artur-Alexandrov <arturches@gmail.com>
2019-10-04 13:50:56 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function zeros(expression) {
   // your solution
+  
+  let a = expression.split("*");
+  
+  
+  let twos = 0;
+  let fives = 0;
+  let step = 0;
+  
+
+  a.forEach(element => {if(element.includes("!!")) {
+
+    element = parseInt(element);
+    step = 2;
+    
+  } else {element = parseInt(element)
+    step = 1;
+    
+   }
+
+    
+    while(element >= 1){
+
+      let five = element;
+      let two = element;
+  
+      while(five % 5 === 0){
+        fives++;
+        five /= 5;
+      }
+  
+      while(two % 2 === 0){
+        twos++;
+        two /= 2; 
+      }
+      element = element -  step;
+    }
+  }
+    );
+
+ 
+  if (twos > fives) {
+    return fives;
+  } else return twos;
 }
